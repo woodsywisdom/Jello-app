eab4f530fc05d81ca2fe4de22fafada2eaa955f5
Matthew Taylor <matthewtaylor9758@gmail.com>
2020-09-30 12:26:39 -0400

    add a grid

--- client/src/pages/Boards.js
+++ client/src/pages/Boards.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Container, IconButton, } from '@material-ui/core';
 
+
+
+
+const useStyles = makeStyles(( theme ) => {
+
+});
 
 const Boards = () => {
+  const classes = useStyles();
 
   return (
-    <>
-      <h1>Your Boards Will Be Here Someday, but not soon</h1>
-    </>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+
+      </Grid>
+    </div>
   )
 }
 
